fb7c6d081164fdb6ce519c0d278efc9c51d62e74
Mario Orellana <mario@journeyhero.net>
2022-04-22 16:10:52 -0600

    Add dynamic port

--- app.js
+++ app.js
@@ -69,4 +69,8 @@ app.get("/anime/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {console.log("Server running!")});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {console.log("Server running!")});
