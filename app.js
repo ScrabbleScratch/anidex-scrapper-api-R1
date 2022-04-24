e5b5e99610e6233ea94a5a906b1277e499d2d19e
Mario Orellana <mario@journeyhero.net>
2022-04-23 21:36:21 -0600

    Add single request

--- app.js
+++ app.js
@@ -14,11 +14,47 @@ const Jikan = axios.create({
 const dbUri = process.env.DB_HOST;
 const client = new MongoClient(dbUri);
 
-client.connect();
+//INSERT DATA FROM SINGLE REQUEST
+async function insertSingleData(category, doc) {
+    try {
+        await client.connect();
+        const database = client.db(process.env.DB_NAME);
+        const collection = database.collection(category);
 
+        const filter = {"mal_id": doc.mal_id}
+
+        let response = {
+            success: false,
+            operation: null,
+            id: null
+        };
+
+        const existing = await collection.findOne(filter);
+        if (existing) {
+            const queryResult = await collection.findOneAndReplace(filter, doc);
+            response = {
+                success: queryResult.lastErrorObject.updatedExisting,
+                operation: "update",
+                id: queryResult.value._id
+            };
+        } else {
+            const queryResult = await collection.insertOne(doc);
+            response = {
+                success: queryResult.acknowledged,
+                operation: "insert",
+                id: queryResult.insertedId
+            };
+        }
+        return response;
+    } finally {
+        await client.close();
+    }
+}
+
+// INSERT DATA FROM BATCH REQUEST
 async function insertBatchData(category, docs) {
     try {
-        // await client.connect();
+        await client.connect();
         const database = client.db(process.env.DB_NAME);
         const collection = database.collection(category);
 
@@ -63,11 +99,56 @@ async function insertBatchData(category, docs) {
         }
         return response;
     } finally {
-        // await client.close();
+        await client.close();
     }
 }
 
 const availableCategories = ["anime", "characters", "manga"];
+
+// SINGLE REQUEST
+app.get("/:category/:id", (req, res) => {
+    const category = req.params.category.toLowerCase();
+    if (availableCategories.includes(category)) {
+        const current = category.toUpperCase() + ": " + req.params.id + ": ";
+        Jikan.get(category + "/" + req.params.id).then(async ({status, data}) => {
+            if ("data" in data) {
+                console.log(current + status);
+                const response = await insertSingleData(category, data.data).catch(console.dir);
+                res.status(status);
+                res.send(response);
+            } else {
+                if ("status" in data) {
+                    console.log(current + data.status);
+                    res.status(data.status);
+                    res.send(data);
+                } else {
+                    console.log(current + 501);
+                    res.status(501);
+                    res.send(data);
+                }
+            }
+            
+        }).catch((err) => {
+            if (err.response) {
+                console.log(current + err.response.status);
+                res.status(err.response.status);
+                res.send(err.response.data);
+            } else {
+                console.log(err);
+                res.status(400);
+                res.send(err);
+            }
+        });
+    } else {
+        console.log("Unknown category");
+        response = {
+            status_code: 400,
+            error: "Unknown category!"
+        };
+        res.status(400);
+        res.send(response);
+    }
+});
 
 // BATCH REQUEST
 app.get("/batch/:category/:page", (req, res) => {
@@ -115,7 +196,6 @@ app.get("/batch/:category/:page", (req, res) => {
         res.status(400);
         res.send(response);
     }
-    
 });
 
 let port = process.env.PORT;
