2f7f71bce36b9ca2ad1f4cf721fac662b87edb3b
Mario Orellana <mario@journeyhero.net>
2022-04-27 18:26:01 -0600

    Base commit for V4. Removed batch and details request, added single detail category request and added avaiolable details per category request

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const dbUri = process.env.DB_HOST;
 const client = new MongoClient(dbUri);
 
 // CHECK EXISTANCE OF AN ENTRY WITHIN DATABASE
-async function checkExistance(version, category, id) {
+async function checkExistance(version, category, id, db) {
     try {
         await client.connect();
-        const database = client.db(process.env.DB_NAME + "_" + version);
+        const database = client.db(db ?? (process.env.DB_NAME + "_" + version));
         const collection = database.collection(category);
 
         const filter = {"mal_id": parseInt(id)};
@@ -36,11 +36,11 @@ async function checkExistance(version, category, id) {
 }
 
 // GET ALL mal_id IN A COLLECTION
-async function getMALIds(version, category) {
+async function getMALIds(version, category, db) {
     let existingIds = [];
     try {
         await client.connect()
-        const database = client.db(process.env.DB_NAME + "_" + version);
+        const database = client.db(db ?? (process.env.DB_NAME + "_" + version));
         const collection = database.collection(category);
 
         existingIds = await collection.distinct("mal_id");
@@ -51,7 +51,7 @@ async function getMALIds(version, category) {
 }
 
 // INSERT DATA FROM SINGLE REQUEST
-async function insertSingleData(version, category, id, doc) {
+async function insertSingleData(version, category, id, doc, db) {
     let response = {
         success: false,
         operation: null,
@@ -59,7 +59,7 @@ async function insertSingleData(version, category, id, doc) {
     };
     try {
         await client.connect();
-        const database = client.db(process.env.DB_NAME + "_" + version);
+        const database = client.db(db ?? (process.env.DB_NAME + "_" + version));
         const collection = database.collection(category);
 
         const filter = {"mal_id": parseInt(id)};
@@ -87,11 +87,11 @@ async function insertSingleData(version, category, id, doc) {
 }
 
 // INSERT DATA FROM BATCH REQUEST
-async function insertBatchData(version, category, docs) {
+async function insertBatchData(version, category, docs, db) {
     let response = [];
     try {
         await client.connect();
-        const database = client.db(process.env.DB_NAME + "_" + version);
+        const database = client.db(db ?? (process.env.DB_NAME + "_" + version));
         const collection = database.collection(category);
 
         const existingIds = await collection.distinct("mal_id");
@@ -140,26 +140,38 @@ async function insertBatchData(version, category, docs) {
     return response;
 }
 
-const availableVersions = ["v3", "v4"];
-const availableCategories = {
-    v3: ["anime", "character", "manga"],
-    v4: ["anime", "characters", "manga"]
+const availableVersions = {
+    v3: {
+        anime: [],
+        character: [],
+        manga: []
+    },
+    v4: {
+        anime: ["characters", "staff", "pictures", "statistics", "recommendations", "relations", "themes", "external"],
+        characters: ["anime", "manga", "voices", "pictures"],
+        manga: ["characters", "pictures", "statistics", "recommendations", "relations", "external"]
+    }
 };
 
 // GET mal_id'S REQUEST
 app.get("/:version/:category", async (req, res) => {
     let response;
     const version = req.params.version;
-    if (availableVersions.includes(version)) {
+    if (version in availableVersions) {
         const category = req.params.category.toLowerCase();
-        if (availableCategories[version].includes(category)) {
-            const availableIds = await getMALIds(version, category);
+        if (category in availableVersions[version]) {
+            const db = req.query.db;
+            const availableIds = await getMALIds(version, category, db);
             const current = category.toUpperCase() + ": IDS: " + availableIds.length;
             console.log(current);
-            response = availableIds;
+            response = {
+                success: true,
+                data: availableIds
+            };
         } else {
             console.log("Unknown category");
             response = {
+                success: false,
                 status_code: 400,
                 error: "Unknown category!"
             };
@@ -168,6 +180,41 @@ app.get("/:version/:category", async (req, res) => {
     } else {
         console.log("Unknown version");
         response = {
+            success: false,
+            status_code: 400,
+            error: "Unknown version!"
+        };
+        res.status(response.status_code);
+    }
+    res.send(response);
+});
+
+// GET AVAILABLE CATEGORY INFO
+app.get("/:version/:category/info", async (req, res) => {
+    let response;
+    const version = req.params.version;
+    if (version in availableVersions) {
+        const category = req.params.category.toLowerCase();
+        if (category in availableVersions[version]) {
+            const current = category.toUpperCase() + ": INFO: " + availableVersions[version][category].length;
+            console.log(current);
+            response = {
+                success: true,
+                data: availableVersions[version][category]
+            }
+        } else {
+            console.log("Unknown category");
+            response = {
+                success: false,
+                status_code: 400,
+                error: "Unknown category!"
+            };
+            res.status(response.status_code);
+        }
+    } else {
+        console.log("Unknown version");
+        response = {
+            success: false,
             status_code: 400,
             error: "Unknown version!"
         };
@@ -180,19 +227,22 @@ app.get("/:version/:category", async (req, res) => {
 app.get("/:version/:category/:id", async (req, res) => {
     let response;
     const version = req.params.version;
-    if (availableVersions.includes(version)) {
+    if (version in availableVersions) {
         const category = req.params.category.toLowerCase();
-        const id = req.params.id;
-        if (availableCategories[version].includes(category)) {
+        if (category in availableVersions[version]) {
+            const id = req.params.id;
+            const db = req.query.db;
             const current = category.toUpperCase() + ": " + id + ": ";
             await Jikan.get(version+"/"+category+"/"+id).then(async ({status, data}) => {
                 if (version === "v3" && "mal_id" in data) {
                     console.log(current + status);
-                    response = await insertSingleData(version, category, id, data).catch(console.dir);
+                    response = await insertSingleData(version, category, id, data, db).catch(console.dir);
+                    response.data = data;
                     res.status(status);
                 } else if (version === "v4" && "data" in data) {
                     console.log(current + status);
-                    response = await insertSingleData(version, category, id, data.data).catch(console.dir);
+                    response = await insertSingleData(version, category, id, data.data, db).catch(console.dir);
+                    response.data = data.data;
                     res.status(status);
                 } else {
                     if ("status" in data) {
@@ -218,6 +268,7 @@ app.get("/:version/:category/:id", async (req, res) => {
         } else {
             console.log("Unknown category");
             response = {
+                success: false,
                 status_code: 400,
                 error: "Unknown category!"
             };
@@ -226,6 +277,7 @@ app.get("/:version/:category/:id", async (req, res) => {
     } else {
         console.log("Unknown version");
         response = {
+            success: false,
             status_code: 400,
             error: "Unknown version!"
         };
@@ -234,67 +286,70 @@ app.get("/:version/:category/:id", async (req, res) => {
     res.send(response);
 });
 
-app.get("/:version/:category/:id/details", async (req, res) => {
+app.get("/:version/:category/:id/:info", async (req, res) => {
     let response;
     const version = req.params.version;
-    if (version === "v4") {
+    if (version in availableVersions) {
         const category = req.params.category.toLowerCase();
-        const id = req.params.id;
-        if (availableCategories[version].includes(category)) {
-            let current = category.toUpperCase() + " DETAILS: " + id + ": ";
-            if (await checkExistance(version, category, id)) {
-                let availableDetails;
-                switch (category) {
-                    case "anime":
-                        availableDetails = ["characters", "staff", "pictures", "statistics", "recommendations", "relations", "themes", "external"];
-                        break;
-                    case "characters":
-                        availableDetails = ["anime", "manga", "voices", "pictures"];
-                        break;
-                    case "manga":
-                        availableDetails = ["characters", "pictures", "statistics", "recommendations", "relations", "external"];
-                        break;
-                    default:
-                        availableDetails = [];
-                        break;
-                }
-                
-                // FUNCTION TO MAKE A PAUSE
-                function sleep(ms) {
-                    return new Promise((resolve) => {
-                    setTimeout(resolve, ms);
-                    });
-                }
-
-                const doc = {};
-                for (let i=0; i < availableDetails.length; i++) {
-                    const detail = availableDetails[i];
-                    await Jikan.get(version+"/"+category+"/"+id+"/"+detail).then(async ({status, data}) => {
-                        if ("data" in data) {
-                            if ("status" in data) {
-                                current += " " + data.status;
-                            } else {
-                                current += " " + status;
-                                doc[detail+"_data"] = data.data;
+        if (category in availableVersions[version]) {
+            const info = req.params.info;
+            if (availableVersions[version][category].includes(info)) {
+                const id = req.params.id;
+                const db = req.query.db;
+                if (await checkExistance(version, category, id, db)) {
+                    const current = category.toUpperCase() + ": " + info.toUpperCase() + ": " + id + ": ";
+                    await Jikan.get(version+"/"+category+"/"+id+"/"+info).then(async ({status, data}) => {
+                        if (version === "v4" && "data" in data) {
+                            console.log(current + status);
+                            const doc = {
+                                [info+"_data"]: data.data
                             }
+                            response = await insertSingleData(version, category, id, doc, req.query.db).catch(console.dir);
+                            response.data = doc;
+                            res.status(status);
                         } else {
-                            current += " " + 501;
+                            if ("status" in data) {
+                                console.log(current + data.status);
+                                res.status(data.status);
+                            } else {
+                                console.log(current + 501);
+                                res.status(501);
+                            }
+                            response = data;
+                        }
+                    }).catch((err) => {
+                        if (err.response) {
+                            console.log(current + err.response.status);
+                            res.status(err.response.status);
+                            response = err.response.data;
+                        } else {
+                            console.log(err);
+                            res.status(400);
+                            response = err;
                         }
                     });
-                    await sleep(1500);
-                };
-                console.log(current);
-                response = await insertSingleData(version, category, id, doc);
+                } else {
+                    console.log("Id not found in DB");
+                    response = {
+                        success: false,
+                        status_code: 406,
+                        error: "Id not found within database. Add it before requesting details!"
+                    };
+                    res.status(response.status_code);
+                }
             } else {
+                console.log("Unknown info");
                 response = {
-                    status_code: 406,
-                    error: "Id not found within database. Add it before requesting details!"
+                    success: false,
+                    status_code: 400,
+                    error: "Unknown info!"
                 };
                 res.status(response.status_code);
             }
         } else {
             console.log("Unknown category");
             response = {
+                success: false,
                 status_code: 400,
                 error: "Unknown category!"
             };
@@ -303,6 +358,7 @@ app.get("/:version/:category/:id/details", async (req, res) => {
     } else {
         console.log("Unknown version");
         response = {
+            success: false,
             status_code: 400,
             error: "Unknown version!"
         };
@@ -312,62 +368,62 @@ app.get("/:version/:category/:id/details", async (req, res) => {
 });
 
 // BATCH REQUESTS
-app.get("/:version/batch/:category/:page", async (req, res) => {
-    let response;
-    const version = req.params.version;
-    if (version === "v4") {
-        const category = req.params.category.toLowerCase();
-        if (availableCategories[version].includes(category)) {
-            const page = req.params.page;
-            const current = category.toUpperCase() + " PAGE: " + page + ": ";
-            await Jikan.get(version+"/"+category+"?page="+page).then(async ({status, data}) => {
-                if ("data" in data && "pagination" in data) {
-                    console.log(current + status);
-                    const dbRS = await insertBatchData(version, category, data.data).catch(console.dir);
-                    response = {
-                        pagination: data.pagination,
-                        response: dbRS
-                    }
-                    res.status(status);
-                } else {
-                    if ("status" in data) {
-                        console.log(current + data.status);
-                        res.status(data.status);
-                    } else {
-                        console.log(current + 501);
-                        res.status(501);
-                    }
-                    response = data;
-                }
-            }).catch((err) => {
-                if (err.response) {
-                    console.log(current + err.response.status);
-                    res.status(err.response.status);
-                    reponse = err.response.data;
-                } else {
-                    console.log(err);
-                    res.status(400);
-                    response = err;
-                }
-            });
-        } else {
-            console.log("Unknown category");
-            response = {
-                status_code: 400,
-                error: "Unknown category!"
-            };
-            res.status(response.status_code);
-        }
-    } else {
-        console.log("Unknown version");
-        response = {
-            status_code: 400,
-            error: "Unknown version!"
-        };
-        res.status(response.status_code);
-    }
-    res.send(response);
-});
+// app.get("/:version/:category/batch/:page", async (req, res) => {
+//     let response;
+//     const version = req.params.version;
+//     if (version === "v4") {
+//         const category = req.params.category.toLowerCase();
+//         if (category in availableVersions[version]) {
+//             const page = req.params.page;
+//             const current = category.toUpperCase() + " PAGE: " + page + ": ";
+//             await Jikan.get(version+"/"+category+"?page="+page).then(async ({status, data}) => {
+//                 if ("data" in data && "pagination" in data) {
+//                     console.log(current + status);
+//                     const dbRS = await insertBatchData(version, category, data.data, req.query.db).catch(console.dir);
+//                     response = {
+//                         pagination: data.pagination,
+//                         response: dbRS
+//                     }
+//                     res.status(status);
+//                 } else {
+//                     if ("status" in data) {
+//                         console.log(current + data.status);
+//                         res.status(data.status);
+//                     } else {
+//                         console.log(current + 501);
+//                         res.status(501);
+//                     }
+//                     response = data;
+//                 }
+//             }).catch((err) => {
+//                 if (err.response) {
+//                     console.log(current + err.response.status);
+//                     res.status(err.response.status);
+//                     reponse = err.response.data;
+//                 } else {
+//                     console.log(err);
+//                     res.status(400);
+//                     response = err;
+//                 }
+//             });
+//         } else {
+//             console.log("Unknown category");
+//             response = {
+//                 status_code: 400,
+//                 error: "Unknown category!"
+//             };
+//             res.status(response.status_code);
+//         }
+//     } else {
+//         console.log("Unknown version");
+//         response = {
+//             status_code: 400,
+//             error: "Unknown version!"
+//         };
+//         res.status(response.status_code);
+//     }
+//     res.send(response);
+// });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
